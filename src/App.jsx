4fab788e9b51393a40fe07096c967d3ed9fc5d96
Abhijith N B <www.abhijithbose@gmail.com>
2023-06-18 12:47:30 +0530

    updated from dev again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-    return <div>This a test is updated in dev branch</div>;
+    return <div>This a test is updated in dev branch twice</div>;
 }
 
 export default App;
